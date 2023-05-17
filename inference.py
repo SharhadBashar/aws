339952403e2de143c0a8d42a130586d7b9679c7c
sharhad <sharhad.bashar@uwaterloo.ca>
2023-05-17 12:02:44 -0400

    added new functionality as well as tests to upload and host models on sagemaker

---
+++ inference.py
@@ -0,0 +1,34 @@
+"""
+Deserialize fitted model
+"""
+def model_fn(model_dir):
+    model = joblib.load(os.path.join(model_dir, 'model.joblib'))
+    return model
+
+"""
+input_fn
+    request_body: The body of the request sent to the model.
+    request_content_type: (string) specifies the format/variable type of the request
+"""
+def input_fn(request_body):
+    if (type(request_body) == str):
+        return [request_body]
+    else:
+        raise ValueError('This model only supports string inputs')
+
+"""
+predict_fn
+    input_data: returned array from input_fn above
+    model (sklearn model) returned model loaded from model_fn above
+"""
+def predict_fn(input_data, model):
+    return model.predict(input_data)[0]
+
+"""
+output_fn
+    prediction: the returned value from predict_fn above
+    content_type: the content type the endpoint expects to be returned. Ex: JSON, string
+
+"""
+def output_fn(prediction):
+    return int(prediction[0])
